47af6f7520a428df3c72afbe535a811cec567bf4
siebrum <siebrenkraak@hotmail.com>
2018-09-26 10:25:45 +0200

    Add "pay" functionality

--- DiscordBotAPI/Controllers/UsersController.cs
+++ DiscordBotAPI/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web.Http;
 using DiscordBotAPI.Mapping;
 using DiscordBotAPI.Services;
+using TNSApi.Mapping;
 
 namespace DiscordBotAPI.Controllers
 {
@@ -49,6 +50,44 @@ namespace DiscordBotAPI.Controllers
             users = users.GetRange(0, 5);
 
             return Ok(users);
+        }
+
+        [HttpPost]
+        [Route("api/pay")]
+        public IHttpActionResult PayPerson([FromBody] Coinflip request)
+        {
+            var donator = _database.Users.Where(x => x.DiscordId == request.Challenger.DiscordId).FirstOrDefault();
+            var receiver = _database.Users.Where(x => x.DiscordId == request.Enemy.DiscordId).FirstOrDefault();
+
+            if(donator == null)
+            {
+                request.Result = CoinflipVsResults.ChallengeDoesntExist;
+                return Ok(request);
+            }
+
+            if (receiver == null)
+            {
+                request.Result = CoinflipVsResults.EnemyWon;
+                return Ok(request);
+            }
+
+            if (donator.Points < request.Points)
+            {
+                request.Challenger = donator;
+                request.Enemy = receiver;
+                request.Result = CoinflipVsResults.ChallengerNoPoints;
+                return Ok(request);
+            }
+
+            request.Result = CoinflipVsResults.ChallengerWon;
+            donator.Points -= request.Points;
+            receiver.Points += request.Points;
+            _database.Context.SaveChanges();
+
+            request.Challenger = donator;
+            request.Enemy = receiver;
+
+            return Ok(request);
         }
 
         [HttpPost]
